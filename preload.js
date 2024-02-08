698590545c07e891f89e609263ef93ab27b0dfd0
권금련 <jinlian.quan@atto-research.com>
2024-02-08 19:29:01 +0900

    contextBridge 이용하여 window객체에 렌더링전에 데이터 저장하기

--- preload.js
+++ preload.js
@@ -4,7 +4,17 @@ window.addEventListener('DOMContentLoaded', () => {
     if (element) element.innerText = text
   }
 
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency])
-  }
+  // for (const dependency of ['chrome', 'node', 'electron']) {
+  //   replaceText(`${dependency}-version`, process.versions[dependency])
+  // }
+})
+
+
+const { contextBridge } = require('electron')
+
+// preload window 객체에 원하는 데이터 저장하기
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron 
 })
--- render.js
+++ render.js
@@ -1,1 +1,5 @@
 console.log("여기 비지니스 로직 쓰기?")
+
+
+const information = document.getElementById('info')
+information.innerText = `현재 사용되고 있는것은 Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), 와 Electron (v${versions.electron()})`
