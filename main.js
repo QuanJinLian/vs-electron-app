ececf930aca1a424a394973943322d76c9ba14ed
권금련 <jinlian.quan@atto-research.com>
2024-02-11 16:40:52 +0900

    작업바에 커스텀 아이콘 설정 테스트

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, BrowserWindow, ipcMain, Tray , nativeImage} = require('electron');
 const path = require('node:path');
 
 
 
 const createWindow = () => {
+  const _path = path.join(__dirname, '32.png');
+  const appIcon = new Tray(_path)
+  const img =  nativeImage.createFromPath(_path, {width: 16, height: 16})
+
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: img,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
@@ -17,7 +22,7 @@ const createWindow = () => {
 
 
   // 개발툴 열어 놓기
-  mainWindow.webContents.openDevTools();
+  win.webContents.openDevTools();
 }
 
 
